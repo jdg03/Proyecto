20537beed64740b28d0ca2697b07c419dd5af87a
JackeVillalobos <jackelinevillalobos11@gmail.com>
2025-02-08 15:55:05 -0600

    Formulario del login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,21 +1,80 @@
-import Link from 'next/link';
+import Link from "next/link";
+import * as React from "react";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import styles from "./login.module.css";
 
-function login() {
-    
-    return (
-        <div className="">
-            <h1>login</h1>
-            <p>Welcome to the login</p>
-            <div>
-           
-        <Link href="/inicio/inventario">
-            <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                Iniciar
+function Login() {
+  return (
+    <div className={styles["login-container"]}>
+      <div className={styles["login-box"]}>
+        {/* Sección izquierda con el logo */}
+        <div className={styles["logo-container"]}>
+          <img
+            src=".../"
+            alt=""
+            className={styles["login-logo"]}
+          />
+        </div>
+
+        {/* Sección derecha con el formulario */}
+        <div className={styles["form-container"]}>
+          <h1 className={styles["form-title"]}>Bienvenido a Taller ****</h1>
+          <p className={styles["form-subtitle"]}>
+            Ingrese sus credenciales para iniciar sesión.
+          </p>
+
+          <form className="w-full max-w-md mt-6 space-y-5">
+            {/* Usuario */}
+            <div className={styles["input-field"]}>
+              <label htmlFor="user" className={styles["input-label"]}>
+                Usuario
+              </label>
+              <div className="relative">
+                <UserOutlined className={styles["input-icon"]} />
+                <input
+                  type="text"
+                  id="user"
+                  name="user"
+                  className={styles["input"]}
+                  placeholder="Ingrese su usuario"
+                />
+              </div>
+            </div>
+
+            {/* Contraseña */}
+            <div className={styles["input-field"]}>
+              <label htmlFor="password" className={styles["input-label"]}>
+                Contraseña
+              </label>
+              <div className="relative mb-6">
+                <LockOutlined className={styles["input-icon"]} />
+                <input
+                  id="password"
+                  name="password"
+                  className={styles["input"]}
+                  placeholder="Ingrese su contraseña"
+                  type="password"
+                />
+              </div>
+            </div>
+
+            {/* Botón de inicio de sesión */}
+            <button type="submit" className={styles["submit-button"]}>
+              Iniciar Sesión
             </button>
-        </Link>
+
+            {/* Enlace de registro */}
+            <div className={styles["register-link-container"]}>
+              <p className="text-gray-600">¿No tienes una cuenta? </p>
+              <Link href="/register">
+              <span className="ml-2 text-[#780000]  hover:underline cursor-pointe">Regístrate</span>
+              </Link>
+            </div>
+          </form>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default login;
+export default Login;
